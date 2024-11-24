062f606793b51cd3149e717d9a2fa41581b0ed21
benuhdrill <bgmach2023@fau.edu>
2024-11-24 15:36:24 -0500

    Initial Commit

---
+++ BarterUp/BarterUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarterUpApp.swift
+//  BarterUp
+//
+//  Created by Ben Gmach on 11/24/24.
+//
+
+import SwiftUI
+
+@main
+struct BarterUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
