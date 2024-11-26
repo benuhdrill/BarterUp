b21feb7e0687f8ff86193a1f37db073f403bf9a6
benuhdrill <bgmach2023@fau.edu>
2024-11-26 16:06:31 -0500

    incorporated messages

---
+++ BarterUp/MessageView.swift
@@ -0,0 +1,18 @@
+//
+//  MessageView.swift
+//  BarterUp
+//
+//  Created by Ben Gmach on 11/26/24.
+//
+
+import SwiftUI
+
+struct MessageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MessageView()
+}
---
+++ BarterUp/MessagesView.swift
@@ -0,0 +1,18 @@
+//
+//  MessagesView.swift
+//  BarterUp
+//
+//  Created by Ben Gmach on 11/26/24.
+//
+
+import SwiftUI
+
+struct MessagesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MessagesView()
+}
