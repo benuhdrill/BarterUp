bf15ef2d9c983afb65b602073db729f5b2e8d84a
benuhdrill <bgmach2023@fau.edu>
2024-11-26 14:07:42 -0500

    HomeView updated for a sleeker look

---
+++ BarterUp/AuthenticationManager.swift
@@ -0,0 +1,18 @@
+//
+//  AuthenticationManager.swift
+//  BarterUp
+//
+//  Created by Ben Gmach on 11/26/24.
+//
+
+import SwiftUI
+
+struct AuthenticationManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthenticationManager()
+}
