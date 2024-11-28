b90153de0f8a65ddc306a6425eff9ce550d896db
benuhdrill <bgmach2023@fau.edu>
2024-11-28 12:34:16 -0500

    Favorites tab fixed, added message notification

---
+++ BarterUp/FavoritesView.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesView.swift
+//  BarterUp
+//
+//  Created by Ben Gmach on 11/28/24.
+//
+
+import SwiftUI
+
+struct FavoritesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoritesView()
+}
