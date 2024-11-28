da81524e78859c2ae7b6709f6fcd8770aa46b2e8
benuhdrill <bgmach2023@fau.edu>
2024-11-28 14:47:10 -0500

    Fixed UI

---
+++ BarterUp/ViewComponents.swift
@@ -0,0 +1,18 @@
+//
+//  ViewComponents.swift
+//  BarterUp
+//
+//  Created by Ben Gmach on 11/28/24.
+//
+
+import SwiftUI
+
+struct ViewComponents: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewComponents()
+}
