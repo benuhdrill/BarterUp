52c79a166de17a8e90bfcca5381f98dcbec8c378
benuhdrill <bgmach2023@fau.edu>
2024-11-28 13:17:41 -0500

    Saving

---
+++ BarterUp/Models.swift
@@ -0,0 +1,18 @@
+//
+//  Models.swift
+//  BarterUp
+//
+//  Created by Ben Gmach on 11/28/24.
+//
+
+import SwiftUI
+
+struct Models: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Models()
+}
