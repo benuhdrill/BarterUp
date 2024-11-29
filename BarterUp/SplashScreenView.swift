baecebdb1a7a2ba5c70830fdbc4f47d058c16274
benuhdrill <bgmach2023@fau.edu>
2024-11-29 11:01:08 -0500

    Saved Changes

---
+++ BarterUp/SplashScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreenView.swift
+//  BarterUp
+//
+//  Created by Ben Gmach on 11/28/24.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}
